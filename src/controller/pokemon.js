a7ce60047653544da646e3567f8dc03cf23a5227
ramos <rdiego26@gmail.com>
2017-06-05 00:26:56 -0300

    MODIFY structure buy method .

--- src/controller/pokemon.js
+++ src/controller/pokemon.js
@@ -16,49 +16,60 @@ const controller = {
 
 	buy: function(req, res) {
 
-		dao.findOne({
-			where: {
-				name: req.body.name
-			}
-		})
+		const _filter = {
+			name: req.body.name
+		};
+
+		dao.findOne(_filter)
 		.then(function(pokemon) {
-			if (pokemon.stock < req.body.quantity) {
-				return res.status(400).send({
+
+			if (!pokemon) {
+
+				res.status(400).send({
+					error: 'Pokémon not registered!'
+				});
+
+			} else if (!pokemon ||  pokemon.stock < req.body.quantity) {
+
+				res.status(400).send({
 					error: 'Not enought ' + pokemon.name + ' in stock: ' + pokemon.stock
+				});
+
+			} else {
+
+				request({
+					uri: 'https://api.pagar.me/1/transactions',
+					method: 'POST',
+					json: {
+						api_key: "ak_test_WHgSu2XFmvoopAZMetV3LfA2RfEEQg",
+						amount: pokemon.price * req.body.quantity * 100,
+						card_number: "4024007138010896",
+						card_expiration_date: "1050",
+						card_holder_name: "Ash Ketchum",
+						card_cvv: "123",
+						metadata: {
+							product: 'pokemon',
+							name: pokemon.name,
+							quantity: req.body.quantity
+						}
+					}
 				})
+					.then(function (body){
+						if (body.status == 'paid') {
+
+							pokemon.stock = pokemon.stock - req.body.quantity;
+							pokemon.save()
+								.then(function(pokemon) {
+									res.send(body);
+								})
+						}
+					})
+					.catch(function (err){
+						console.log(JSON.stringify(err, null, 4));
+						res.status(err.response.statusCode).send(err.response.body);
+					})
+
 			}
-
-			request({
-				uri: 'https://api.pagar.me/1/transactions',
-				method: 'POST',
-				json: {
-					api_key: "ak_test_WHgSu2XFmvoopAZMetV3LfA2RfEEQg",
-					amount: pokemon.price * req.body.quantity * 100,
-					card_number: "4024007138010896",
-					card_expiration_date: "1050",
-					card_holder_name: "Ash Ketchum",
-					card_cvv: "123",
-					metadata: {
-						product: 'pokemon',
-						name: pokemon.name,
-						quantity: req.body.quantity
-					}
-				}
-			})
-				.then(function (body){
-					if (body.status == 'paid') {
-
-						pokemon.stock = pokemon.stock - req.body.quantity;
-						pokemon.save()
-							.then(function(pokemon) {
-								res.send(body);
-							})
-					}
-				})
-				.catch(function (err){
-					console.log(JSON.stringify(err, null, 4));
-					return res.status(err.response.statusCode).send(err.response.body);
-				})
 
 		});
 
