64cbac648f1cf3d12e32e12c4c02d639335716fe
ramos <rdiego26@gmail.com>
2017-06-04 22:37:30 -0300

    EXTERNALIZE pokemon model .

---
+++ src/model/pokemon.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const sequelize = require(path.resolve('src/utils/connection'));
+const Sequelize = require('sequelize');
+
+const pokemon = sequelize.define('pokemon', {
+	name: {
+		type: Sequelize.STRING,
+		allowNull: false
+	},
+	price: {
+		type: Sequelize.INTEGER,
+		allowNull: false
+	},
+	stock: {
+		type: Sequelize.INTEGER,
+		allowNull: true,
+		defaultValue: 1
+	}
+});
+
+module.exports = pokemon;
