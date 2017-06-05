1377894e0313a822580684832394d8eaf4cc5e3e
ramos <rdiego26@gmail.com>
2017-06-04 23:58:48 -0300

    ADD helmet .

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ const constants = require(path.resolve('src/utils/constants'));
 const express = require('express');
 const pokemonRoutes = require(path.resolve('src/routes/pokemon'));
 const bodyParser = require('body-parser');
+const helmet = require('helmet');
 
 /* App Configuration */
 const app = express();
 app.set('port', constants.server.port);
 app.set('title', constants.app.name);
 app.use(bodyParser.json());
+app.use(helmet());
 
 pokemonRoutes(app);
 
