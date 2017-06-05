907357482d5b28d00c4b2fe344d20dfe08d49b23
ramos <rdiego26@gmail.com>
2017-06-04 22:32:11 -0300

    EXTERNALIZE connection .

---
+++ src/utils/connection.js
@@ -0,0 +1,9 @@
+const path = require('path');
+const Sequelize = require('sequelize');
+const constants = require(path.resolve('src/utils/constants'));
+const sequelize = new Sequelize(constants.db.name, null, null, {
+	dialect: 'sqlite',
+	logging: false
+});
+
+module.exports = sequelize;
