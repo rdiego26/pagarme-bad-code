9c87ee073a35a003306c288b4782a586e60d621a
rdiego26 <rdiego26@gmail.com>
2017-06-28 18:06:31 -0300

    USE schema on create .

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -5,9 +5,12 @@ module.exports = function(app) {
 	const constants = require(path.resolve('src/utils/constants'));
 	const pokemonController = require(path.resolve('src/controller/pokemon'));
 	const router = require('express').Router();
+    const validate = require('express-jsonschema').validate;
+    const createSchema = require(path.resolve('src/schema/createPokemon.json'));
+
 
 	router.get('/pokemons', pokemonController.getAll);
-	router.put('/pokemons', pokemonController.create);
+	router.put('/pokemons', validate({body: createSchema}), pokemonController.create);
 	router.post('/pokemons', pokemonController.buy);
 
 	app.use(router);
