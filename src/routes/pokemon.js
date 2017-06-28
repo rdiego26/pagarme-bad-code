705cb4c84bd34895a367a8d9973383322f6e86f6
ramos <rdiego26@gmail.com>
2017-06-28 20:38:29 -0300

    USE schema on buy pokemon .

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -5,13 +5,14 @@ module.exports = function(app) {
 	const constants = require(path.resolve('src/utils/constants'));
 	const pokemonController = require(path.resolve('src/controller/pokemon'));
 	const router = require('express').Router();
-    const validate = require('express-jsonschema').validate;
-    const createSchema = require(path.resolve('src/schema/createPokemon.json'));
+  const validate = require('express-jsonschema').validate;
+  const createSchema = require(path.resolve('src/schemas/createPokemon.json'));
+	const buySchema = require(path.resolve('src/schemas/buyPokemon.json'));
 
 
 	router.get('/pokemons', pokemonController.getAll);
 	router.put('/pokemons', validate({body: createSchema}), pokemonController.create);
-	router.post('/pokemons', pokemonController.buy);
+	router.post('/pokemons', validate({body: buySchema}), pokemonController.buy);
 
 	app.use(router);
 };
