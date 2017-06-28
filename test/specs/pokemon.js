052db437a4ed676e800a5407bb2b42c6be07a28c
ramos <rdiego26@gmail.com>
2017-06-28 20:46:14 -0300

    UPDATE spec tests .

--- test/specs/pokemon.js
+++ test/specs/pokemon.js
@@ -1,5 +1,6 @@
 const path = require('path'),
 	assert = require('chai').assert,
+	R = require('ramda'),
 	mocks = require(path.resolve('test/mocks')),
 	model = require(path.resolve('src/model/pokemon')),
 	dao = require(path.resolve('src/dao/pokemon'));
@@ -9,40 +10,25 @@ describe('Pokémon Spec', function() {
 	const _fullPokemon = mocks.fullValidToCreate;
 	const _simplePokemon = mocks.simpleValidToCreate;
 
-	before(function(done) {
-		model.sync({force: true}).then(function () {
-			dao.deleteOne({name: _fullPokemon.name}).then(function() {
-				done();
-			}).catch(function(err) {
-				console.error(err);
-			});
-		});
-
-	});
-
-	after(function(done) {
-		dao.deleteOne({name: _fullPokemon.name}).then(function() {
-
-			dao.deleteOne({name: _simplePokemon.name}).then(function() {
-				done();
-			}).catch(function(err) {
-				console.error(err);
-			});
-
-		}).catch(function(err) {
-			console.error(err);
+	beforeEach(function(done) {
+		model.sync({force: true}).then(function() {
+			done();
 		});
 	});
 
 	it('should create full valid pokémon', function(done) {
 
-		dao.create(_fullPokemon).then(function() {
+		dao.create(_fullPokemon).then(function(created) {
+			assert.isObject(created);
+			assert.property(created, 'id');
 			done();
 		});
 	});
 
 	it('should create simple valid pokémon', function(done) {
-		dao.create(_simplePokemon).then(function() {
+		dao.create(_simplePokemon).then(function(created) {
+			assert.isObject(created);
+			assert.property(created, 'id');
 			done();
 		});
 	});
@@ -88,6 +74,18 @@ describe('Pokémon Spec', function() {
 		}).catch(function() {
 			done();
 		});
+	});
+
+	it('not should create a duplicated pokémon', function(done) {
+		dao.create(_fullPokemon).then(function() {
+			dao.create(_fullPokemon).then(function() {
+			}).catch(function(error) {
+				assert.isObject(error);
+				assert.ok(R.contains('name', R.pluck('path', error.errors)));
+				done();
+			});
+		});
+
 	});
 
 });
