59c0fcd50f6b39f7fe4162a976ae9c9d89bed637
ramos <rdiego26@gmail.com>
2017-06-29 01:56:52 -0300

    USE double quotes .

--- src/utils/time.js
+++ src/utils/time.js
@@ -1,16 +1,16 @@
-'use strict';
+"use strict";
 
 module.exports = {
 
 	formatSeconds: function(secs) {
 		function pad(s) {
-			return (s < 10 ? '0' : '') + s;
+			return (s < 10 ? "0" : "") + s;
 		}
 		const hours = Math.floor(secs / (60*60));
 		const minutes = Math.floor(secs % (60*60) / 60);
 		const seconds = Math.floor(secs % 60);
 
-		return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+		return pad(hours) + ":" + pad(minutes) + ":" + pad(seconds);
 	}
 
 };
