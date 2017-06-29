e620e448e7e22be4b25cd5c23b317a6145bd19c8
ramos <rdiego26@gmail.com>
2017-06-29 01:27:37 -0300

    ADD time util.

---
+++ src/utils/time.js
@@ -0,0 +1,16 @@
+'use strict';
+
+module.exports = {
+
+	formatSeconds: function(secs) {
+		function pad(s) {
+			return (s < 10 ? '0' : '') + s;
+		}
+		const hours = Math.floor(secs / (60*60));
+		const minutes = Math.floor(secs % (60*60) / 60);
+		const seconds = Math.floor(secs % 60);
+
+		return pad(hours) + ':' + pad(minutes) + ':' + pad(seconds);
+	}
+
+};
