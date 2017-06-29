26f557a2370fe45c7687451bd2edaa4ce72ab2c1
ramos <rdiego26@gmail.com>
2017-06-29 01:56:06 -0300

    SMALL change .

--- src/routes/pokemon.js
+++ src/routes/pokemon.js
@@ -1,14 +1,14 @@
 'use strict';
 
-module.exports = function(app) {
-	const path = require('path');
-	const constants = require(path.resolve('src/utils/constants'));
-	const pokemonController = require(path.resolve('src/controller/pokemon'));
-	const router = require('express').Router();
-  const validate = require('express-jsonschema').validate;
-  const createSchema = require(path.resolve('src/schemas/createPokemon.json'));
-	const buySchema = require(path.resolve('src/schemas/buyPokemon.json'));
+const path = require('path');
+const constants = require(path.resolve('src/utils/constants'));
+const pokemonController = require(path.resolve('src/controller/pokemon'));
+const router = require('express').Router();
+const validate = require('express-jsonschema').validate;
+const createSchema = require(path.resolve('src/schemas/createPokemon.json'));
+const buySchema = require(path.resolve('src/schemas/buyPokemon.json'));
 
+module.exports = function(app) {
 
 	router.get('/pokemons', pokemonController.getAll);
 	router.put('/pokemons', validate({body: createSchema}), pokemonController.create);
